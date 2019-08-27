e1f3ad7f134491580aab9b36286e84f92a443f64
Felipe Campara <fcamparasilva@gmail.com>
2019-08-26 21:31:05 -0400

    Feat (Models): Create model to user

--- api/src/routes.js
+++ api/src/routes.js
@@ -1,9 +1,15 @@
-import { Router } from 'express';
+import { Router } from 'express'
+import User from './app/models/User'
 
-const routes = new Router();
+const routes = new Router()
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello Word' });
-});
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Felipe',
+    email: 'fcampara@gmail.com',
+    password_hash: '1223456'
+  })
+  return res.json(user)
+})
 
 export default routes
