8c2cb4079cb2ec08b36096246376ef4db7c3c98b
Felipe Campara <fcamparasilva@gmail.com>
2019-09-02 22:28:08 -0400

    Feat (First module): Finish first module

--- api/src/routes.js
+++ api/src/routes.js
@@ -1,15 +1,16 @@
 import { Router } from 'express'
-import User from './app/models/User'
+
+import UserController from './app/controllers/UserController'
+import SessionController from './app/controllers/SessionController'
+
+import authMiddleware from './app/middlewares/auth'
 
 const routes = new Router()
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Felipe',
-    email: 'fcampara@gmail.com',
-    password_hash: '1223456'
-  })
-  return res.json(user)
-})
+routes.post('/users', UserController.store)
+routes.post('/session', SessionController.store)
+
+routes.use(authMiddleware)
+routes.put('/users', UserController.update)
 
 export default routes
